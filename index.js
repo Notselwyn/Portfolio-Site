6b6ceeb93514eb560dd1e4f967604b15449ccca3
Notselwyn <68616630+Notselwyn@users.noreply.github.com>
2021-06-30 19:21:09 +0200

    Added filters to projects page and added rickroll

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// EXPRESS SETTINGS
 let express = require('express');
 let app = express();
 
@@ -7,21 +8,22 @@ app.use("/static/js", express.static('static/js'));
 app.set('view engine', 'pug');
 app.set('views','./views/pages');
 
-// Constants
+// CONSTANTS
 const projects = {
    "Finished": {
       "sudoku_solver": {
          "title": "Sudoku Solver",
          "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus quam id leo in vitae turpis massa sed elementum. Tincidunt augue interdum velit euismod in. Pulvinar sapien et ligula ullamcorper. In iaculis nunc sed augue lacus viverra vitae congue eu. Ipsum suspendisse ultrices gravida dictum fusce ut. Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Venenatis urna cursus eget nunc. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Fusce ut placerat orci nulla pellentesque dignissim enim sit amet. Aliquet eget sit amet tellus cras adipiscing enim. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Amet mauris commodo quis imperdiet. Sit amet mattis vulputate enim nulla. Aliquet eget sit amet tellus cras adipiscing enim. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Amet mauris commodo quis imperdiet. Sit amet mattis vulputate enim nulla. Aliquet eget sit amet tellus cras adipiscing enim. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Amet mauris commodo quis imperdiet. Sit amet mattis vulputate enim nulla.",
          "github": "https://github.com/Notselwyn/SudokuSolver",
+         "demo": "sudoku",
          "banner": "/static/img/sudoku_solver.gif",
-         "tags": ["C++", "Go", "JavaScript", "Python", "Ruby", "Algorithm"]},
+         "tags": ["CPP", "Go", "JavaScript", "Python", "Ruby", "Algorithm"]},
       "asciify": {
          "title": "ASCII Filter",
          "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus quam id leo in vitae turpis massa sed elementum. Tincidunt augue interdum velit euismod in. Pulvinar sapien et ligula ullamcorper. In iaculis nunc sed augue lacus viverra vitae congue eu. Ipsum suspendisse ultrices gravida dictum fusce ut. Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Venenatis urna cursus eget nunc. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Fusce ut placerat orci nulla pellentesque dignissim enim sit amet. Aliquet eget sit amet tellus cras adipiscing enim. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Amet mauris commodo quis imperdiet. Sit amet mattis vulputate enim nulla. Aliquet eget sit amet tellus cras adipiscing enim. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Amet mauris commodo quis imperdiet. Sit amet mattis vulputate enim nulla. Aliquet eget sit amet tellus cras adipiscing enim. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Amet mauris commodo quis imperdiet. Sit amet mattis vulputate enim nulla.",
          "github": "https://github.com/Notselwyn/ASCII-Footage/blob/main/ascii.cpp",
          "banner": "",
-         "tags": ["C++", "Python"]
+         "tags": ["CPP", "Python"]
       }
    },
    "In Development": {
@@ -36,7 +38,7 @@ const projects = {
          "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus quam id leo in vitae turpis massa sed elementum. Tincidunt augue interdum velit euismod in. Pulvinar sapien et ligula ullamcorper. In iaculis nunc sed augue lacus viverra vitae congue eu. Ipsum suspendisse ultrices gravida dictum fusce ut. Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Venenatis urna cursus eget nunc. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Fusce ut placerat orci nulla pellentesque dignissim enim sit amet. Aliquet eget sit amet tellus cras adipiscing enim. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Amet mauris commodo quis imperdiet. Sit amet mattis vulputate enim nulla. Aliquet eget sit amet tellus cras adipiscing enim. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Amet mauris commodo quis imperdiet. Sit amet mattis vulputate enim nulla. Aliquet eget sit amet tellus cras adipiscing enim. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam. Amet mauris commodo quis imperdiet. Sit amet mattis vulputate enim nulla.",
          "github": "https://github.com/Notselwyn/Portfolio-Site",
          "banner": "",
-         "tags": ["CSS", "HTML", "Javascript"]
+         "tags": ["CSS", "HTML", "JavaScript"]
       }
    },
    "Coming Soon": {},
@@ -55,15 +57,62 @@ app.use(function(req, res, next) {
    next();
 });
 
+// PAGE REGISTRATION
 app.get(['/', '/index'], function(req, res){
    let args = {"url": req.url, "browser_title": "Home", "title": titles["index"], "subtitles": subtitles["index"]};
    res.render('index', args);
 });
 
 app.get('/projects', function(req, res){
-   let args = {"url": req.url, "browser_title": "Projects", "title": titles["projects"], "subtitles": subtitles["projects"], "projects": projects};
+   let tags = null;
+   if ("filter" in req.query) {
+      tags = req.query["filter"].split(" ");
+      if (tags[0] === '') {
+         tags = null;
+      } else {
+         tags = tags.filter((e, i) => tags.indexOf(e) === i).sort();
+      }
+   }
+   let args = {"url": req.url, "browser_title": "Projects", "title": titles["projects"], "subtitles": subtitles["projects"], "projects": projects, "tags": tags};
    res.render('projects', args);
 });
 
+app.get(['/admin', '/dashboard', '/secret', '/config', '/hidden', '/code_exec'], function(req, res) {
+   res.render('rickroll')
+});
 
-app.listen(8080, );
+
+// PENTESTING
+app.get("/pentest", function(req, res) {
+   return res.send({"/pentest/xss": "perform xss"})
+});
+
+app.get("/pentest/xss", function(req, res) {
+   return res.render('pentest/xss')
+});
+
+
+// API
+app.get("/api", function(req, res) {
+   res.status(200);
+   return res.send({"/api/calc": "solve a math problem (Parameter p)"})
+});
+
+app.get("/api/calc", function(req, res) {
+   if ("p" in req.query) {
+      let p = req.query["p"].replace(" ", "+")
+      if (/^[0-9()+\-*\/]+$/.test(p)) {
+         res.status(200);
+         return res.send({"solution": eval(p)});
+      } else {
+         res.status(500);
+         return res.send({"solution": "Internal Error."});
+      }
+   } else {
+      res.status(500);
+      return res.send({"solution": "Parameter (p) not included."});
+   }
+});
+
+// START WEBSERVER
+app.listen(979, "urmom");
