17f81d55418f03693e727f01a3a902cbb23e27ff
notselwyn <laurencewit4@gmail.com>
2021-09-05 15:45:18 +0200

    thank wakatime

--- index.js
+++ index.js
@@ -247,12 +247,16 @@ app.get("/api/wakatime_text", function(req, res) {
       let username = req.query["username"];
       if (/^[a-zA-Z0-9_-]+$/.test(username)) {
          fetch(`https://wakatime.com/api/v1/users/${username}/stats?is_including_today=true`).then(wakatime_res => wakatime_res.text()).then(wakatime_body => {   
-            if ("!DOCTYPE" in wakatime_body || "error" in JSON.parse(wakatime_body) || !('"categories"' in wakatime_body)) {
+            if ("!DOCTYPE" in wakatime_body || "error" in JSON.parse(wakatime_body)) {
                res.status(500);
                return res.send("Internal Error.");
             }
             wakatime_body = wakatime_body.replace("Visual Studio", "VS 2019");
             let data = JSON.parse(wakatime_body)["data"];
+            if (data["categories"] == undefined) {
+               res.status(500);
+               return res.send("Internal Error.");
+            }
             let coding_info;
             data = data;
             for (let i=0; i<data["categories"].length; i++) {
