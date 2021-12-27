0b315ad9e5170dc09312c1431d04954712c594df
notselwyn <laurencewit4@gmail.com>
2021-12-27 13:59:28 +0100

    Added dotenv

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { Router, Route } from "svelte-navigator";
-    //import * as dotenv from "dotenv";
+    import * as dotenv from "dotenv";
 
     import Home from "./routes/Home.svelte";
     import Projects from "./routes/Projects.svelte";
@@ -9,10 +9,9 @@
 
     import WrapperHead from "./lib/WrapperHead.svelte";
 
-    //dotenv.config();
+    dotenv.config();
 
-    //let url = process.env.PUB_URL;
-    let url = "http://localhost:3000";
+    let url = process.env.PUB_URL;
     console.log(url);
 </script>
 
