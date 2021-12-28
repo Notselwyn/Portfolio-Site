068704f75231686fc712878247dde5d3ccbaee96
notselwyn <laurencewit4@gmail.com>
2021-12-28 23:09:59 +0100

    addedd logging

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -38,6 +38,7 @@
     <TextTitle pagename={"About Me"} title={["About ", "!Me"]} subtitle={["About Me", "My Skills", "My Interests"]} />
 
     {#await data_promise then data}
+        <p> {data} </p>
         {#each data as {index, post}} 
             <SlateToSvelte document={post} />
         {/each}
